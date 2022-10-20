37585bec16862a1c65f8d332fcbe997df1d34e04
TartCodes <tartcodes@gmail.com>
2022-10-20 14:44:00 -0400

    very early

---
+++ pf2e-manager/src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Pathfinder 2e Character Manager</h1>          
+  </div>
+  )
+}
+
+export default Header
---
+++ pf2e-manager/src/components/Welcome.js
@@ -0,0 +1,12 @@
+
+
+const Welcome = () => {
+  return (
+    <div>
+        <p>Welcome! This is a simple character manager for Pathfinder 2nd Edition using React.js. You can find the first iteration <a target="_blank" rel="noreferrer" href="https://character-manager-pf2e.herokuapp.com/">here</a>, which is using vanilla JS along with Node/Express.js, EJS, and MongoDB.
+        </p>
+    </div>
+  )
+}
+
+export default Welcome
