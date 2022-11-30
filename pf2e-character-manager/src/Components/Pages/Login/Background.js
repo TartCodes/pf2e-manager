b7ce77155098dcf069c9f8a7d70de464e8277a46
TartCodes <tartcodes@gmail.com>
2022-11-29 23:37:31 -0500

    login page style

---
+++ pf2e-character-manager/src/Components/Pages/Login/Background.js
@@ -0,0 +1,9 @@
+const Background = () => {
+  return (
+    <div className="bg-image">
+      <img src="/assets/pf2e-resized.jpg" alt="" />
+    </div>
+  );
+};
+
+export default Background;
--- pf2e-character-manager/src/Components/Pages/Login/Login.js
+++ pf2e-character-manager/src/Components/Pages/Login/Login.js
@@ -1,13 +1,15 @@
 import "./Login.css";
 import Header from "./Header";
+import Background from "./Background";
 
 const Login = () => {
   return (
-    <div>
+    <div className="main-wrapper">
       <section className="wrapper">
         <Header />
         <div className="login">Login</div>
       </section>
+      <Background />
     </div>
   );
 };
