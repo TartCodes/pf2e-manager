fd87fae91afd8d8fc4ebb08640d9d0f4dedaf40b
TartCodes <tartcodes@gmail.com>
2022-12-15 13:49:36 -0500

    test

--- pf2e-character-manager/src/App.js
+++ pf2e-character-manager/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-// import Login from "./Components/Pages/Login/Login";
+import Login from "./Components/Pages/Login/Login";
 import SelectCharacter from "./Components/Pages/SelectCharacter/SelectCharacter";
 
 const App = () => {
   return (
     <Router>
       <div className="container">
-        <SelectCharacter />
+        <Login />
+        {/* <SelectCharacter /> */}
       </div>
     </Router>
   );
